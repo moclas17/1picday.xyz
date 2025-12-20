9efcc72447bee002a139cf3abb3c56c0257aeceb
Erik Valle <erik.valle@gmail.com>
2025-12-20 15:29:34 -0600

    fix: stripe initialization during build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const apiKey = process.env.STRIPE_SECRET_KEY || "sk_test_build_time_placeholder";
+
+export const stripe = new Stripe(apiKey, {
+    // @ts-ignore - version might not be in types yet if very recent
     apiVersion: '2025-12-15.clover',
     typescript: true,
 });
